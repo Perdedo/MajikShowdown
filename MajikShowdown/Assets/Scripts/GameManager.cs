b99f9645ef4cd9741e9ba7d3b17d57cb61c1ef91
Matucao <lucasmatuck@gmail.com>
2026-03-21 20:03:04 -0300

    SpellTriggers Events

--- MajikShowdown/Assets/Scripts/GameManager.cs
+++ MajikShowdown/Assets/Scripts/GameManager.cs
@@ -8,8 +8,7 @@ public class GameManager : MonoBehaviour
     public List<Player> Players = new List<Player>();
     public NetworkAuxiliarControl netCtrl;
 
-    public HexGrid TestGrid;
-    public SpellCollider ProjectilePrefab;
+    
     void Awake()
     {
         if(Instance == null)
@@ -38,16 +37,5 @@ public class GameManager : MonoBehaviour
             netCtrl.ffManager.gameObject.SetActive(false);
         }
     }
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            InstantiateSpellCollider(TestGrid.spell.SubSpells[0],Vector3.up);
-        }
-    }
-    public void InstantiateSpellCollider(SubSpell subSpell, Vector3 pos)
-    {
-        GameObject g = Instantiate(ProjectilePrefab.gameObject, pos, Quaternion.identity);
-        g.GetComponent<SpellCollider>().subSpell = subSpell;
-    }
+    
 }
