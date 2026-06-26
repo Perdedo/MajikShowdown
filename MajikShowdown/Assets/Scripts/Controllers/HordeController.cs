773c060dad09845915af559ccd50748df3467739
Perdedo <rafaeltdilascio@gmail.com>
2026-06-25 21:40:15 -0300

    w

--- MajikShowdown/Assets/Scripts/Controllers/HordeController.cs
+++ MajikShowdown/Assets/Scripts/Controllers/HordeController.cs
@@ -189,8 +189,8 @@ public class HordeController : NetworkBehaviour
                     enemiesInfo.Add(new EnemyTransformInfo(aux[i].enemy, aux[i].pos, aux[i].scale, aux[i].rot));
                 }
                 enemiesInfo[i].SetValues(aux[i].enemy, aux[i].pos, aux[i].scale, aux[i].rot, Time.time, aux[i].vel);
-                aux[i].enemy.transform.position = aux[i].pos;
-                aux[i].enemy.transform.rotation = aux[i].rot;
+                //aux[i].enemy.transform.position = aux[i].pos;
+                //aux[i].enemy.transform.rotation = aux[i].rot;
                 //aux[i].enemy.transform.localScale = aux[i].scale;
             }
         }
