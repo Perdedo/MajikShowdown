ee49ca324f22d4f7ffd5b68e31c8b6701236b249
Matucao <lucasmatuck@gmail.com>
2026-08-25 17:36:34 -0300

    merge

--- MajikShowdown/Assets/Scripts/Characters/Enemy/Enemy.cs
+++ MajikShowdown/Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -877,7 +877,13 @@ public unsafe struct EnemyFieldLocation : IJobParallelFor
     [Unity.Collections.ReadOnly] public NativeArray<int> CellNeighbors;
     public int maxEnemiesPerCell;
     public int maxEnemyOccupiedCells;
-    [Unity.Collections.ReadOnly]public NativeArray<CellJobData> Cells;
+    [Unity.Collections.ReadOnly] public NativeArray<CellJobData> Cells;
+    
+    public float3 flowfieldOffset;
+    public float CellSize;
+    [Unity.Collections.ReadOnly] public NativeArray<int> CellCollumFirst;
+    [Unity.Collections.ReadOnly] public NativeArray<int> CellCollumCount;
+    public int ColumWidthValue;
 
     //Prompted && output
     public NativeArray<EnemyJobData> EnemyData;
@@ -913,18 +919,26 @@ public unsafe struct EnemyFieldLocation : IJobParallelFor
         }
         TargetIndices[index] = closestPlayerIndex;
     }
-    public int WorldToGridPosition(float3 worldPosition)
+    public int WorldToGridPosition(float3 worldPosition, bool ToLowerOnly = true)
     {
         int closest = -1;
         float closestDist = float.MaxValue;
-        for (int i = 0; i < Cells.Length; i++)
+        float3 localPos = worldPosition + flowfieldOffset;
+        int2 coords = new int2((int)math.floor(localPos.x / CellSize), (int)math.floor(localPos.z / CellSize));
+        int columIndex = (coords.x * ColumWidthValue) + coords.y;
+        for (int i = 0; i < CellCollumCount[columIndex]; i++)
         {
-            float newDistance = math.distance(worldPosition, Cells[i].Position);
+            int cellindex = i + CellCollumFirst[columIndex];
+            float newDistance = math.abs(worldPosition.y - Cells[cellindex].Position.y);
             if (newDistance < closestDist)
             {
-                closestDist = newDistance;
-                closest = i;
+                if (!ToLowerOnly || Cells[cellindex].Position.y <= worldPosition.y)
+                {
+                    closestDist = newDistance;
+                    closest = cellindex;
+                }
             }
+
         }
         return closest;
     }
